c891a5ea8ac0384069320382f2d5c0003a90f35b
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-04-07 13:20:48 +0200

    correction

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-        //chiedere all'utente il suo nome
-        const username = prompt(`Scrivi qui il tuo nome`)
-        console.log(username);
+//chiedere all'utente il suo nome
+const username = prompt("Scrivi qui il tuo nome")
+console.log(username);
 
-        //chiedere all'utente il suo cognome
-        const usersurname = prompt(`Scrivi qui il tuo cognome`)
-        console.log(usersurname);
+//chiedere all'utente il suo cognome
+const usersurname = prompt("Scrivi qui il tuo cognome")
+console.log(usersurname);
 
-        //chiedere all'utente il suo colore preferito
-        const color = prompt(`Scrivi qui il tuo colore preferito`)
-        console.log(color);
+//chiedere all'utente il suo colore preferito
+const color = prompt("Scrivi qui il tuo colore preferito")
+console.log(color);
 
-        //Creare la password
-        const message = `La tua nuova password è ${username}${usersurname}${color}21`;
-        console.log(message);
+//Creare la password
+const message = `La tua nuova password è ${username}${usersurname}${color}21`;
+console.log(message);
 
-        //Scrivere la password sulla pagina
-        document.getElementById('password').innerHTML = message ;
+//Scrivere la password sulla pagina
+document.getElementById('password').innerHTML = message ;
